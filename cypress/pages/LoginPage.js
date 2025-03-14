acf29fb56415cdeb2237e05b85065b92cbdcf7b1
ganeshtamil2003 <ganesh28062003@gmail.com>
2025-03-14 08:59:36 +0530

    Code updated

--- cypress/pages/LoginPage.js
+++ cypress/pages/LoginPage.js
@@ -2,25 +2,27 @@ const TIMEOUT = 5000;
 
 class LoginPage {
   elements = {
-    usernameInput: () => cy.get('#loginusername', { timeout: TIMEOUT }),
-    passwordInput: () => cy.get('#loginpassword', { timeout: TIMEOUT }),
+    usernameInput: () => cy.get("#loginusername", { timeout: TIMEOUT }),
+    passwordInput: () => cy.get("#loginpassword", { timeout: TIMEOUT }),
     LoginButton: () =>
-    cy.get('.modal-footer > .btn-primary').eq(2).click({ force: true }),
+      cy.get(".modal-footer > .btn-primary").eq(2).click({ force: true }),
   };
 
   openLogin() {
-    cy.get('#login2').click()
-    cy.wait(500)
+    cy.get("#login2").click();
+    cy.wait(500);
     // cy.get("#signInModal", { timeout: TIMEOUT }).should("be.visible");
-    cy.get('#logInModal > .modal-dialog > .modal-content > .modal-body').should("be.visible")
+    cy.get("#logInModal > .modal-dialog > .modal-content > .modal-body").should(
+      "be.visible"
+    );
   }
 
   login(username, password) {
     this.openLogin();
     cy.wait(1000);
-    this.elements.usernameInput().should('be.visible').type(username);
-    this.elements.passwordInput().should('be.visible').type(password);
-    this.elements.LoginButton().should('be.enabled').click();
+    this.elements.usernameInput().should("be.visible").type(username);
+    this.elements.passwordInput().should("be.visible").type(password);
+    this.elements.LoginButton().should("be.enabled").click();
   }
 
   verifyAlertMessage(expectedText) {
@@ -30,4 +32,4 @@ class LoginPage {
   }
 }
 
-
+export default new LoginPage();
